fe91a2a59c43c990eb481c371b42a1b461c5b188
Denn1593 <oowej@hotmail.com>
2020-11-24 15:46:17 +0100

    chat almost done

--- src/main/java/com/example/bratwurst/model/Message.java
+++ src/main/java/com/example/bratwurst/model/Message.java
@@ -7,9 +7,31 @@ public class Message
     private int id;
     private int sender;
     private int receiver;
+    private String senderUsername;
+    private String receiverUsername;
     private String content;
     private String timestamp;
     private boolean file;
+
+    public String getSenderUsername()
+    {
+        return senderUsername;
+    }
+
+    public void setSenderUsername(String senderUsername)
+    {
+        this.senderUsername = senderUsername;
+    }
+
+    public String getReceiverUsername()
+    {
+        return receiverUsername;
+    }
+
+    public void setReceiverUsername(String receiverUsername)
+    {
+        this.receiverUsername = receiverUsername;
+    }
 
     public String getTimestamp()
     {
