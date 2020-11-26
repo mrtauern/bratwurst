c49582eb71c1ad77bfe63d67aea2a299ad5e7969
Denn1593 <oowej@hotmail.com>
2020-11-26 10:47:48 +0100

    stuff

--- src/main/java/com/example/bratwurst/controller/MessageController.java
+++ src/main/java/com/example/bratwurst/controller/MessageController.java
@@ -1,6 +1,7 @@
 package com.example.bratwurst.controller;
 
 import com.example.bratwurst.model.Message;
+import com.example.bratwurst.model.User;
 import com.example.bratwurst.service.FileService;
 import com.example.bratwurst.service.MessageService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.http.HttpSession;
 import java.util.List;
 
 @RestController
@@ -22,23 +24,60 @@ public class MessageController
     private FileService fileService;
 
     @GetMapping("/{sender}/{receiver}")
-    public List<Message> getConversation(@PathVariable int sender, @PathVariable int receiver)
+    public ResponseEntity<List<Message>> getConversation(@PathVariable int sender, @PathVariable int receiver, HttpSession session)
     {
-        return messageService.getConversation(sender, receiver);
+        try
+        {
+            User user = (User) session.getAttribute("login");
+            if(sender != user.getId() || user.getId() == receiver)
+            {
+                return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+
+            }
+            return new ResponseEntity<>(messageService.getConversation(sender, receiver), HttpStatus.OK);
+        }
+        catch (NullPointerException e)
+        {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
     }
 
     @PostMapping("/new")
-    public ResponseEntity postMessage(@RequestBody Message message)
+    public ResponseEntity postMessage(@RequestBody Message message, HttpSession session)
     {
-        messageService.postMessage(message);
+        try
+        {
+            User user = (User) session.getAttribute("login");
+            if (message.getSender() != user.getId() || user.getId() == message.getReceiver())
+            {
+                return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+            }
+            messageService.postMessage(message);
 
-        return new ResponseEntity(HttpStatus.OK);
+            return new ResponseEntity(HttpStatus.OK);
+        }
+        catch (NullPointerException e)
+        {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
     }
 
     @PostMapping("/new/file/{sender}/{receiver}")
-    public ResponseEntity uploadFile(@PathVariable int sender, @PathVariable int receiver, @RequestParam MultipartFile file)
+    public ResponseEntity uploadFile(@PathVariable int sender, @PathVariable int receiver, @RequestParam MultipartFile file, HttpSession session)
     {
-        fileService.saveFileWithMessage(sender, receiver, file);
-        return new ResponseEntity(HttpStatus.OK);
+        try
+        {
+            User user = (User) session.getAttribute("login");
+            if (sender != user.getId() || user.getId() == receiver)
+            {
+                return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+            }
+            fileService.saveFileWithMessage(sender, receiver, file);
+            return new ResponseEntity(HttpStatus.OK);
+        }
+        catch (NullPointerException e)
+        {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
     }
 }
